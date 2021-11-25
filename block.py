75bbf253f97b1db2a6d377b230fafb7859f09dca
gcf <guidocarlo.ferrante@gmail.com>
2021-11-25 21:59:20 +0100

    Update block.py
    Using `nonce` instead of `key`.

--- block.py
+++ block.py
@@ -5,14 +5,14 @@ class Block():
         self._hexdigest = _hexdigest  # digest of the previous block (hex)
         self.H = sha256()  # hash object of this block
         self.message = message
-        self.key = 0
+        self.nonce = 0
 
     def __str__(self):
-        return "{}{}{}".format(self._hexdigest, self.message, self.key)
+        return "{}{}{}".format(self._hexdigest, self.message, self.nonce)
 
     def mine(self, difficulty):
         while int(self.H.hexdigest(), 16) > 2**(256 - difficulty):
-            self.key += 1
+            self.nonce += 1
             self.H = sha256()
             bytes_to_hash = str(self).encode('utf-8')  # the encode part converts to bytes
             self.H.update(bytes_to_hash)
